c1e93153c7bac7b1cb90597428e6048b1970edc5
ksk <kuro.kei.mail@gmail.com>
2012-02-16 21:20:27 +0900

    test template

---
+++ src/test/scala/DefaultTest.scala
@@ -0,0 +1,10 @@
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DefaultTest extends FunSuite {
+    test("Default Test"){
+        pending
+    }
+}
